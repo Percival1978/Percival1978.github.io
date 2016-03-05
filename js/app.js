a1063be4974f2de2b9dfc6e72d68e5d713c2f05f
Percival1978 <johnpethers@John-Petherss-MacBook-Pro.local>
2016-03-05 17:29:18 +1100

    Removed old files and replaced with final build

---
+++ js/app.js
@@ -0,0 +1,27 @@
+// Start JQuery
+$(document).ready(function(){
+
+  //  smooth scrolling
+  $('a').click(function(){
+  $("html, body").animate({
+    scrollTop: $( $.attr(this, "href") ).offset().bottom
+  }, 800);
+  return false;
+  });
+
+
+// nav toggle
+$(".button").click(function(){
+  $(".sideNav").toggleClass("hidden");
+});
+
+// spinning button
+$('.button').on('click', function(){
+  $(".navButton").toggleClass('hide');
+});
+
+
+
+
+
+});
